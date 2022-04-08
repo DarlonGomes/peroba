77520d7a6d4408d15436cb8306dadc0de3d97aff
Darlon Gomes <gomesdarlon@icloud.com>
2022-04-08 14:17:57 -0300

    Adição do flip card

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 window.onload = quantasCartas;
 
+
 function quantasCartas(){
     let quantidade = prompt("Bem vindo! Com quantas cartas você quer jogar?")
     quantidade = Number(quantidade)
     while(quantidade % 2 != 0 || quantidade > 14 || quantidade < 4){
         quantidade = prompt("Digite um número par entre 4 e 14.")
     }
-    return quantidade
+    geradorCartas(quantidade);
 }
 
-function distribuir(){
-    quantidade = quantasCartas();
-    geradorCartas(quantidade)
-}
+
 
 function geradorCartas(indice){
     const repositorio = [];
+    const corpo = document.querySelector("main")
     for(i = 0; i < indice / 2; i++){
-        repositorio.push[i]
-        repositorio.push[i]
+        repositorio.push(i);
+        repositorio.push(i);
     }
     
+    for (i = 0; i < indice; i++){
+        corpo.innerHTML += `
+        <div class="card">
+        <div class="front-face face" >
+        <img src="image/front.png" />
+        </div>
+        <div class="back-face face" >
+        <img src="image/fiestaparrot.gif" />
+        </div>
+        </div>
+        `
+    }
 }
+

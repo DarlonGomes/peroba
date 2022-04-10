88c631ef10d2c3a14e8e34a93ea6ce7dad6e594e
Darlon Gomes <gomesdarlon@icloud.com>
2022-04-10 17:37:22 -0300

    Implementação do Timer

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ matriz.sort(comparador);
 let contador = 0;
 let cartaUm;
 let cartaDois;
+let par = 0;
+let total = 0;
+let timer = 0;
 
 function comparador() { 
 	return Math.random() - 0.5; 
@@ -16,6 +19,7 @@ function quantasCartas(){
         quantidade = prompt("Digite um número par entre 4 e 14.")
     }
     geradorCartas(quantidade);
+    timer();
 }
 
 
@@ -25,25 +29,26 @@ function geradorCartas(indice){
     const repositorio = [];
     repositorio.sort(comparador);
     const corpo = document.querySelector("main");
-
-    for(i = 0; i < indice / 2; i++){
+    total += indice
+    for(let i = 0; i < indice / 2; i++){
         repositorio.push(matriz[i]);
         repositorio.push(matriz[i]);
     }
     repositorio.sort(comparador);
     
-    for (i = 0; i < indice; i++){
+    for (let j = 0; j < indice; j++){
         corpo.innerHTML += `
         <div class="card" onclick="rotacionar(this)">
         <div class="front-face face" >
         <img src="image/front.png" />
         </div>
         <div class="back-face face" >
-        <img src="image/${repositorio[i]}.gif" />
+        <img src="image/${repositorio[j]}.gif" />
         </div>
         </div>
         `
     }
+    setInterval(cronometro, 1000);
 }
 
 function retornaOrigem(cartaUm, cartaDois){
@@ -69,6 +74,9 @@ function rotacionar(elemento){
 
     cartaDois = elemento;
 
+    if (cartaUm.innerHTML == cartaDois.innerHTML){
+        par++;
+    }
    const errado = cartaUm.innerHTML != cartaDois.innerHTML;
    if (errado){
        setTimeout(retornaOrigem, 1000, cartaUm, cartaDois);
@@ -76,4 +84,23 @@ function rotacionar(elemento){
 
    cartaUm = undefined;
    cartaDois = undefined;
+   venceu()
+}
+function alerta(){
+    alert(`Você ganhou em ${contador} jogadas e ${timer} segundos`)
+}
+function venceu(){
+    if (par !== total / 2){
+        return;
+    }else{
+        setTimeout(alerta, 1010);
+        
+    }
+
+}
+
+function cronometro(){
+    timer++
+    let temporizador = document.querySelector(".temporizador")
+    temporizador.innerHTML = `${timer}s`
 }
